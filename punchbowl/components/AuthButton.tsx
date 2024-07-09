7504a2499c1e67f8607071068b54373e10e7b6de
AyoNaim Mide <ayonaim101@gmail.com>
2024-07-09 12:43:50 +0000

    made changes

--- punchbowl/components/AuthButton.tsx
+++ punchbowl/components/AuthButton.tsx
@@ -1,18 +1,20 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@supabase/supabase-js";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function AuthButton() {
-  const supabase = createClient();
+  const supabase_url = 'https://dkabkqkvvoiykaouhrvc.supabase.co';
+  const supabase_anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRrYWJrcWt2dm9peWthb3VocnZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg4OTg4MzMsImV4cCI6MjAzNDQ3NDgzM30.Zm5SUO2H4NYHbwi4XksMwWtJq7KO8YpiXFHaHj-89Mg';
+  const supabase = createClient(
+    supabase_url,
+    supabase_anon
+  )
 
   const {
     data: { user },
   } = await supabase.auth.getUser();
 
   const signOut = async () => {
-    "use server";
-
-    const supabase = createClient();
     await supabase.auth.signOut();
     return redirect("/login");
   };
